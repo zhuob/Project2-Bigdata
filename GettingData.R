61e14ee30951c0709eaf021dccae597d9998dd66
Feifei Lei <feifeilei@Feifeis-MacBook-Pro.local>
2014-04-28 14:34:00 -0700

    pushing test

--- GettingData.R
+++ GettingData.R
@@ -9,5 +9,5 @@ password <- "password"
 ontime <- src_postgres("ontime",host = endpoint, port = 5432, user = user, password= password)
 flights <- tbl(ontime, "flights")
 as.tbl(head(flights))
-head(flights)
-tbl_df(head(flights))
+
+

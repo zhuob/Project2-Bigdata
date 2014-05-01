ea59231fd18d485a7ba6a4395429c23e980e003f
zhuob <zhuob@onid.oregonstate.edu>
2014-05-01 15:12:14 -0700

    this is Bin's code part

---
+++ somethingelse.R
@@ -0,0 +1,37 @@
+##  Question to be asked
+
+##------------------------------------------------------------------------------------
+# Are there any airports with flight departure delays that are systematically worse  -
+# or better than other airports? Be sure to consider seasonal fluctuations that might -
+# be due to differential whether patterns, using 25 years of flight data for the 
+# entire country.
+
+
+library(dplyr)
+library(RPostgreSQL)  #added by Feifei
+
+endpoint <- "flights.cwick.co.nz"
+user <- "student"
+password <- "password"
+
+ontime <- src_postgres("ontime", host = endpoint, port = 5432,
+                      user = user, password = password)
+
+flights = tbl(ontime, "flights")
+as.tbl(head(flights))
+
+# dim(flights)  #  154986896        30
+
+hou <- filter(flights, (year == "2011" & month == 1) &  
+                (origin == "SEA" | origin == "IAH") & (weatherdelay == 1))
+head(hou, 20L)
+dim(hou)
+explain(hou)
+
+summerise(group_by(fligts, origin), )
+a <- summarise(group_by(flights, origin), prob = mean(as.integer(depdelay>0)))
+
+collect(a)
+arrange(a, desc(prob))
+
+

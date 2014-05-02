612593f748a5f6366f0649a0886607c13b6c4ee6
MartinGuyer <guyerma@onid.oregonstate.edu>
2014-05-02 08:51:18 -0700

    Since we are not looking at year at the very first I took off selecting it to make a data set that will be quicker.

--- ProportionDelayed.R
+++ ProportionDelayed.R
@@ -1,4 +1,9 @@
-Prop.Delay <- Depart.Delay %.%
+Depart.Delay1 <- flights %.% 
+  select(depdelay, origin) %.%
+  group_by(origin)
+explain(Depart.Delay1)
+
+Prop.Delay <- Depart.Delay1 %.%
   summarise(props = mean(as.integer(depdelay > 0)) %.%
   arrange(desc(props))
   

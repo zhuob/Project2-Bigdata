a73913bd4bf220d56a832f35b2f9becd7540795c
MartinGuyer <guyerma@onid.oregonstate.edu>
2014-05-06 15:05:50 -0700

    Here is the airports with enough flights to be sampled.

--- AirportSamplingAttempt.R
+++ AirportSamplingAttempt.R
@@ -14,6 +14,7 @@ n.05 =  ceiling(((1.96^2)*(Prop.Delay_Data[,2]*(1-Prop.Delay_Data[,2])))/
 n.02 =  ceiling(((1.96^2)*(Prop.Delay_Data[,2]*(1-Prop.Delay_Data[,2])))/
                   (.02^2))
 sample.size_airport = cbind(Prop.Delay_Data, n.01, n.02, n.05)
+sample.size_airport <- arrange(sample.size_airport, origin)
 write.csv(sample.size_airport, "NeedSampleSize.csv", row.names=FALSE)
 
 #That is the numbers we need for each airport. We can from here use 
@@ -35,15 +36,19 @@ Num.flights_year <- flights %.%
 explain(Num.flights_year)
 Num.flights_data = collect(Num.flights_year)
 write.csv(Num.flights_data, "TotalFlights.csv", row.names=FALSE)
+flights.num=Num.flights_data[,2]
+Sample.size_Num.flights <- cbind(sample.size_airport, flights.num)
+write.csv(Sample.size_Num.flights , file="Flights_SampleSize.csv", row.names=FALSE)
 
-Num.Flights_02 <- filter(Num.flights_data, flights > n.02_max)
+Num.Flights_02 <- filter(Sample.size_Num.flights, flights.num > n.02_max)
 num_02 = length(Num.Flights_02[,2])
+write.csv(Num.Flights_02, file="Airports_to_Sample.csv", row.names=FALSE)
 
 origins_02 = Num.Flights_02[,1]
 
 origin_PMD = flights %.%
   filter(origin=="PMD") %.%
-  select(depdelay) %.%
+  select(depdelay, month, year, origin) %.%
   arrange(random())
 explain(origin_PMD)
 
@@ -54,13 +59,13 @@ for(i in 2:num_02){
   airport=toString(origins_02[i])
   origin = flights %.%
     filter(origin==airport) %.%
-    select(depdelay) %.%
+    select(depdelay, month, year, origin) %.%
     arrange(random())
   explain(origin)
   
   sample2=collect(head(origin, n=n.02_max))
   write.csv(sample2, file="sample2.csv", row.names=FALSE)
-  system("cat sample2.csv | sed "1 d" >> combinedSample.csv")
+  system("cat sample2.csv | sed '1 d' >> combinedSample.csv")
 }
 
 cbind(c(1,2

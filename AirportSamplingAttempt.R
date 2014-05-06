a577008786a62d9795f9cab0a31097c66e39487d
MartinGuyer <guyerma@onid.oregonstate.edu>
2014-05-06 15:15:54 -0700

    sampling by 0.02

--- AirportSamplingAttempt.R
+++ AirportSamplingAttempt.R
@@ -38,24 +38,38 @@ Num.flights_data = collect(Num.flights_year)
 write.csv(Num.flights_data, "TotalFlights.csv", row.names=FALSE)
 flights.num=Num.flights_data[,2]
 Sample.size_Num.flights <- cbind(sample.size_airport, flights.num)
+
 write.csv(Sample.size_Num.flights , file="Flights_SampleSize.csv", row.names=FALSE)
+
+
+setwd("/home/zhuob/Project2-Bigdata")
+Sample.size_Num.flights <- read.csv("Flights_SampleSize.csv", header=T)
 
 Num.Flights_02 <- filter(Sample.size_Num.flights, flights.num > n.02_max)
 num_02 = length(Num.Flights_02[,2])
 write.csv(Num.Flights_02, file="Airports_to_Sample.csv", row.names=FALSE)
 
+Num.Flights_02=read.csv("Airports_to_Sample.csv", header=T)
 origins_02 = Num.Flights_02[,1]
+ns_02 = Num.Flights_02[,6]
 
-origin_PMD = flights %.%
-  filter(origin=="PMD") %.%
+origin_1 = toString(origins_02[1])
+
+origin.1 = flights %.%
+  filter(origin==origin_1) %.%
   select(depdelay, month, year, origin) %.%
   arrange(random())
-explain(origin_PMD)
+explain(origin.1)
 
-sample= collect(head(origin_PMD, n=n.02_max))
+n1=as.integer(ns_02[1])
+sample= collect(head(origin.1, n=n1))
+setwd("/home/zhuob/Project2-Bigdata/dataset/")
+
 write.csv(sample, file="combinedSample.csv", row.names=FALSE)
 
-for(i in 2:num_02){
+
+
+for(i in 2:length(ns_02)){
   airport=toString(origins_02[i])
   origin = flights %.%
     filter(origin==airport) %.%
@@ -63,12 +77,16 @@ for(i in 2:num_02){
     arrange(random())
   explain(origin)
   
-  sample2=collect(head(origin, n=n.02_max))
+  n1 = as.integer(ns_02[i])
+  sample2=collect(head(origin, n=n1))
+  
   write.csv(sample2, file="sample2.csv", row.names=FALSE)
   system("cat sample2.csv | sed '1 d' >> combinedSample.csv")
+  system("rm sample2.csv")
 }
 
-cbind(c(1,2
+dddd <- read.csv("combinedSample.csv", header=T)
+
 sample_data = data.frame(samples, col.names=origins_02)
 
 #####################################################################
